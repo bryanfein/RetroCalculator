5a77680867d72e89c0fd735f40b8f2bdacbed4df
bryanfein <bryanfein@gmail.com>
2017-04-01 13:53:23 -0400

    Added UI Elements via autolayout

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -7,19 +7,134 @@
 //
 
 import UIKit
+import AVFoundation //import audio lib
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var btnSound: AVAudioPlayer!
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Mutiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    
+    var currentOperation = Operation.Empty // When the 1st time we open up the Cal we know its empty
+    var runningNumber : String?
+    var leftValStr : String?
+    var rightValStr : String?
+    var result : String?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav") // the audio file is located in the bundle
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        
+        //Error Handling just incase there nothing in the URL (esp when getting audio over the web)
+    
+        do {
+            try btnSound = AVAudioPlayer(contentsOf: soundURL)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        playsound()
+        
+        runningNumber! += "\(sender.tag)" //getting the number tag and assigning it to runningNumber
+        outputLbl.text = runningNumber // assigning runningNumber to the text property to output Label
+        
     }
+    
+    
+    // Buttons Pressed
+    
+    @IBAction func onDividePressed(_ sender: UIButton) {
+        
+        processOperation(operation: .Divide)
+    }
+    
+    @IBAction func onMutiplyPressed(_ sender: UIButton) {
+        
+        processOperation(operation: .Mutiply)
+        
+    }
+    
+    @IBAction func onSubtractPressed(_ sender: UIButton) {
+        
+        processOperation(operation: .Subtract)
+    }
+    
+    @IBAction func onAddPressed(_ sender: UIButton) {
+        
+        
+        processOperation(operation: .Add)
+    }
+    
+    
+    
+    
+    
+    
+    
+    func playsound () {
+        if btnSound.isPlaying {
+            //            btnSound.stop()
+        }
+        btnSound.play()
+    }
+    
+    func processOperation(operation: Operation) {
+        
+        if currentOperation != Operation.Empty {
+            // A user selected an operator, but then selected another operator without first entering a number
+            if runningNumber != nil {
+                rightValStr = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Mutiply {
+                    result = "\(Double (leftValStr!)! * Double(rightValStr!)!)"
+                }else if currentOperation == Operation.Divide {
+                    result = "\(Double (leftValStr!)! / Double(rightValStr!)!)"
+  
+                }else if currentOperation == Operation.Subtract {
+                    result = "\(Double (leftValStr!)! - Double(rightValStr!)!)"
 
+                }else if currentOperation == Operation.Add{
+                    result = "\(Double (leftValStr!)! + Double(rightValStr!)!)"
 
+                }
+                leftValStr = result
+                outputLbl.text = result
+            }
+            
+            currentOperation = operation
+            
+        }else {
+            
+            //This is the first time an operator has been pressed
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
+        
+        
+    }
+    
 }
+
+
 
